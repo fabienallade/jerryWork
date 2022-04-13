151db7d66b4692fb5d06a87f52ff99794bc9f125
Allade fabien <alladefabien@gmail.com>
2022-04-13 12:39:17 +0200

    work on basic validation of form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,11 @@ class WelcomeController < ApplicationController
 
   def create
     p params
+    @data={name:"fabien"}
+    # respond_to do |format|
+    #   # format.html {redirect_to params,notice: "Les informations sont creer"}
+    #   format.json { render json: params}
+    # end
+    render json: @data
   end
 end
